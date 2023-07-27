8562fdb0503116d28d73705e099d06ffd45ef399
Jambickz <jambickz@gmail.com>
2023-07-27 06:42:50 +0300

    Modified app

---
+++ README.md
@@ -0,0 +1,4 @@
+# vite-reactjs-electron
+This is a template for rapid deployment of vite + reactjs + electron app. Used with javascript
+
+Thanks for caumartin. I took his design and modified it to fit me
--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1> Sex </h1>
+			<h1>Vite + ReactJs + Electron</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
